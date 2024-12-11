f18196999a64b5a248f5012e1123d1db41b5b152
M0o4 <indrei457@gmail.com>
2024-12-11 14:30:41 +0300

    fix eslint error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,13 +103,11 @@ export async function updateInvoice(
   redirect("/dashboard/invoices");
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch {
-    return {
-      message: "Database Error: Failed to Delete Invoice",
-    };
+    console.error("Database Error: Failed to Delete Invoice");
   }
 
   revalidatePath("/dashboard/invoices");
